07b49ada8dcafe5f1bbe1adc8a06a5fc9dd52832
Colin Xiong <xiong.colin899@gmail.com>
2023-11-18 15:04:58 -0500

    Working implementation in app.py

--- 123LOADBOARDCHALLENGE/app.py
+++ 123LOADBOARDCHALLENGE/app.py
@@ -1,6 +1,12 @@
 from mqttParser import mqttParser
 from KDTree import KDTree
+from pushNotification import send_notification
 
-kdTree = KDTree()
+FROM_NUMBER = "+16562188280"
 
-mqttParser.mqttParser(kdTree)
+# kdTree = KDTree()
+
+# mqttParser.mqttParser(kdTree)
+
+if __name__ == "__main__":
+    send_notification("Hello world", FROM_NUMBER, "+14384091737")
