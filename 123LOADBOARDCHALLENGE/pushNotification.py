0669f28d237e1f0fa22769238ac3baba709888b6
Colin Xiong <xiong.colin899@gmail.com>
2023-11-18 18:37:45 -0500

    websocket with notifications established

--- 123LOADBOARDCHALLENGE/pushNotification.py
+++ 123LOADBOARDCHALLENGE/pushNotification.py
@@ -1,6 +1,28 @@
 import os
 from twilio.rest import Client
+import asyncio
+from websockets.server import serve
+from websockets.sync.client import connect
 
+connected = set()
+
+async def echo(websocket, path):
+    connected.add(websocket)
+    try:
+        async for message in websocket:
+            for client in connected:
+                # Remove the if condition to send the message back to the sender as well
+                await client.send(message)
+    finally:
+        connected.remove(websocket)
+        print(f"WebSocket connection closed with {websocket.remote_address}")
+
+
+async def main():
+  async with serve(echo, "localhost", 8765):
+    print("WebSocket server is running on ws://localhost:8765")
+    await asyncio.Future()
+      
 
 def send_notification(body, from_, to_):
   account_sid = os.environ['TWILIO_ACCOUNT_SID']
@@ -11,3 +33,6 @@ def send_notification(body, from_, to_):
     .create( body=body, from_=from_, to=to_ )
     
   return message.sid
+
+if __name__ == "__main__":
+ asyncio.run(main())
