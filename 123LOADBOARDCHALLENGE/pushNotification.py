7476a4d5c04c14da4ad636fb7c1c55bb6c9fc5b2
Colin Xiong <xiong.colin899@gmail.com>
2023-11-18 15:03:46 -0500

    pushNotification logic done

---
+++ 123LOADBOARDCHALLENGE/pushNotification.py
@@ -0,0 +1,13 @@
+import os
+from twilio.rest import Client
+
+
+def send_notification(body, from_, to_):
+  account_sid = os.environ['TWILIO_ACCOUNT_SID']
+  auth_token = os.environ['TWILIO_AUTH_TOKEN']
+  client = Client(account_sid, auth_token)
+  
+  message = client.messages \
+    .create( body=body, from_=from_, to=to_ )
+    
+  return message.sid
