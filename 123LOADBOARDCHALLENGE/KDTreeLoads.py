47af39a4b3e25c9ddb52ede9a53ac9632c59281f
Jione <jioneban@gmail.com>
2023-11-18 20:50:09 -0500

    fix kdtreeloads

--- 123LOADBOARDCHALLENGE/KDTreeLoads.py
+++ 123LOADBOARDCHALLENGE/KDTreeLoads.py
@@ -1,13 +1,10 @@
 class KDTree:
     class KDNode:
-        def __init__(self, point, load_id, load_type, time=None, mileage=None, price=None, dest_point=None, left=None, right=None):
-            self.point = point  # (latitude, longitude)
+        def __init__(self, coords, load_id, load_type, load_details, left=None, right=None):
+            self.coords = coords  # (latitude, longitude)
             self.load_id = load_id
             self.load_type = load_type
-            self.time = time
-            self.mileage = mileage
-            self.price = price
-            self.dest_point = dest_point
+            self.load_details = load_details
             self.left = left
             self.right = right
 
@@ -15,64 +12,72 @@ class KDTree:
         self.root = None
         self.load_map = {}
 
-    def insert_load(self, origin_latitude, origin_longitude, load_id, load_type, time=None, mileage=None, price=None, dest_point=None):
-        point = (origin_latitude, origin_longitude)
-
+    def insert_load(self, origin_latitude, origin_longitude, load_id, load_type, load_details):
+        coords = (origin_latitude, origin_longitude)
         if load_id in self.load_map:
             load_node = self.load_map[load_id]
-            load_node.point = point
+            load_node.coords = coords
             load_node.load_type = load_type
-            load_node.time = time
-            load_node.mileage = mileage
-            load_node.price = price
-            load_node.dest_point = dest_point
+            load_node.load_details = load_details
         else:
-            self.root = self._insertKDNode(self.root, point, load_id, load_type, time, mileage, price, dest_point)
-            self.load_map[load_id] = self.root
+            self.root = self._insertKDNode(self.root, coords, load_id, load_type, load_details)
 
     def get_load_by_id(self, load_id):
         return self.load_map.get(load_id)
 
-    def find_k_closest_loads(self, target_point, k=5):
+    def find_k_closest_loads(self, target_coords, k=5):
         closest_loads = []
-        self._find_k_closest_loads(self.root, target_point, closest_loads, k)
-        return closest_loads
+        self._find_k_closest_loads(self.root, target_coords, closest_loads, k)
+        
+        loads = {}  # Dictionary to store load details with load ID as keys
 
-    def _find_k_closest_loads(self, root, target_point, closest_loads, k, depth=0):
+        for node in closest_loads:
+            load_id = node[1].load_id
+            load = {
+                "load_type": node[1].load_type,
+                "coords": node[1].coords,
+                "load_details": node[1].load_details,
+                "distance": node[0]
+            }
+            loads[load_id] = load
+        return loads
+
+    def _find_k_closest_loads(self, root, target_coords, closest_loads, k, depth=0):
         if root is None:
             return
 
-        distance = self._distance(target_point, root.point)
+        distance = self._distance(target_coords, root.coords)
 
         if len(closest_loads) < k:
-            closest_loads.append((-distance, root))
-        elif distance < -closest_loads[0][0]:
-            closest_loads[0] = (-distance, root)
+            closest_loads.append((distance, root))
+        elif distance < closest_loads[k - 1][0]:
+            closest_loads[k - 1] = (distance, root)
         
-        closest_loads.sort(key=lambda x: x[0], reverse=True)  # Sort by distance in descending order
+        closest_loads.sort()  
 
-        cd = depth % 2
+        x_or_y = depth % 2
 
-        if target_point[cd] < root.point[cd]:
-            self._find_k_closest_loads(root.left, target_point, closest_loads, k, depth + 1)
-            if abs(target_point[cd] - root.point[cd]) < -closest_loads[0][0]:
-                self._find_k_closest_loads(root.right, target_point, closest_loads, k, depth + 1)
+        if target_coords[x_or_y] < root.coords[x_or_y]:
+            self._find_k_closest_loads(root.left, target_coords, closest_loads, k, depth + 1)
+            if abs(target_coords[x_or_y] - root.coords[x_or_y]) < closest_loads[k - 1][0]:
+                self._find_k_closest_loads(root.right, target_coords, closest_loads, k, depth + 1)
         else:
-            self._find_k_closest_loads(root.right, target_point, closest_loads, k, depth + 1)
-            if abs(target_point[cd] - root.point[cd]) < -closest_loads[0][0]:
-                self._find_k_closest_loads(root.left, target_point, closest_loads, k, depth + 1)
+            self._find_k_closest_loads(root.right, target_coords, closest_loads, k, depth + 1)
+            if abs(target_coords[x_or_y] - root.coords[x_or_y]) < closest_loads[k - 1][0]:
+                self._find_k_closest_loads(root.left, target_coords, closest_loads, k, depth + 1)
 
-    def _insertKDNode(self, root, point, load_id, load_type, time=None, mileage=None, price=None, dest_point=None, depth=0):
+    def _insertKDNode(self, root, coords, load_id, load_type, load_details, depth=0):
         if root is None:
-            return self.KDNode(point, load_id, load_type, time, mileage, price, dest_point)
-
-        cd = depth % 2
-        if point[cd] < root.point[cd]:
-            root.left = self._insertKDNode(root.left, point, load_id, load_type, time, mileage, price, dest_point, depth + 1)
+            self.load_map[load_id] = self.KDNode(coords, load_id, load_type, load_details)
+            return self.load_map[load_id]
+        
+        x_or_y = depth % 2
+        if coords[x_or_y] < root.coords[x_or_y]:
+            root.left = self._insertKDNode(root.left, coords, load_id, load_type, load_details, depth + 1)
         else:
-            root.right = self._insertKDNode(root.right, point, load_id, load_type, time, mileage, price, dest_point, depth + 1)
+            root.right = self._insertKDNode(root.right, coords, load_id, load_type, load_details, depth + 1)
 
         return root
 
-    def _distance(self, point1, point2):
-        return ((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2) ** 0.5
+    def _distance(self, x1, x2):
+        return ((x1[0] - x2[0]) ** 2 + (x1[1] - x2[1]) ** 2) ** 0.5
